d651633e0da6237ea102928a16afc2156a78c498
Daniel Falbel <dfalbel@gmail.com>
2025-10-10 18:18:14 -0300

    Ingest (#8)
    * add ingest + find_links
    
    * ++ ingest
    
    * use num_workers
    
    * reformat
    
    * ++
    
    * fix tests
    
    * enforce <3.14
    
    * ++

--- src/ragnar/document.py
+++ src/ragnar/document.py
@@ -1,19 +1,11 @@
-from abc import ABC
 from dataclasses import dataclass, field
-from typing import Optional, TypeVar, Generic
-from chonkie.types import Chunk
-
-
-@dataclass
-class Document(ABC):
-    origin: Optional[str]
-    # TODO: currently only text content, but could be an image, audio etc for multimodal models
-    content: str
+from typing import Optional
+from chonkie.types import Chunk, Document
 
 
 @dataclass
 class MarkdownDocument(Document):
-    doc_id: Optional[int] = None
+    origin: Optional[str] = None
 
 
 @dataclass
@@ -25,13 +17,3 @@ class Metric:
 @dataclass
 class RetrievedChunk(Chunk):
     metrics: list[Metric] = field(default_factory=list)
-
-
-DocType = TypeVar("DocType", bound=Document)
-ChunkType = TypeVar("ChunkType", bound=Chunk)
-
-
-@dataclass
-class ChunkedDocument(Generic[DocType, ChunkType]):
-    document: DocType
-    chunks: list[ChunkType]
