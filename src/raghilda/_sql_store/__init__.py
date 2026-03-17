a8acdaca59f9def59b1a10513b39b932d12b60bf
Daniel Falbel <dfalbel@gmail.com>
2026-03-17 12:22:29 -0300

    Narrow _sql_store __init__ to only export PostgresStore

--- src/raghilda/_sql_store/__init__.py
+++ src/raghilda/_sql_store/__init__.py
@@ -1,2 +1,1 @@
-from ._base import SQLStore as SQLStore, build_tables as build_tables, TSVECTOR as TSVECTOR
-from ._postgres import PostgresStore as PostgresStore, PostgresStoreMetadata as PostgresStoreMetadata
+from ._postgres import PostgresStore as PostgresStore
