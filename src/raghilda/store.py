6b86de622fd51b532b58cf8fe4a789284032e601
Daniel Falbel <dfalbel@gmail.com>
2026-03-20 14:42:34 -0300

    Reformat with ruff

--- src/raghilda/store.py
+++ src/raghilda/store.py
@@ -3,7 +3,14 @@ from ._duckdb_store import DuckDBStore
 from ._openai_store import OpenAIStore
 from ._chroma_store import ChromaDBStore
 
-__all__ = ["BaseStore", "WriteResult", "DuckDBStore", "OpenAIStore", "ChromaDBStore", "PostgreSQLStore"]
+__all__ = [
+    "BaseStore",
+    "WriteResult",
+    "DuckDBStore",
+    "OpenAIStore",
+    "ChromaDBStore",
+    "PostgreSQLStore",
+]
 
 try:
     from ._postgres_store import PostgreSQLStore
