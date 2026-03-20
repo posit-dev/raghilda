386b51811b1abc4eda56df231758329ff4bdb012
Daniel Falbel <dfalbel@gmail.com>
2026-03-20 14:33:38 -0300

    Export PostgreSQLStore from the store module
    Import is guarded with try/except so psycopg2 remains optional.

--- src/raghilda/store.py
+++ src/raghilda/store.py
@@ -3,4 +3,9 @@ from ._duckdb_store import DuckDBStore
 from ._openai_store import OpenAIStore
 from ._chroma_store import ChromaDBStore
 
-__all__ = ["BaseStore", "WriteResult", "DuckDBStore", "OpenAIStore", "ChromaDBStore"]
+__all__ = ["BaseStore", "WriteResult", "DuckDBStore", "OpenAIStore", "ChromaDBStore", "PostgreSQLStore"]
+
+try:
+    from ._postgres_store import PostgreSQLStore
+except ImportError:
+    pass
